358bc18a242acc80ff8a84a6d409257ad749cb7e
Cartola <eddie.aluno@gmail.com>
2019-11-01 13:58:58 -0400

    Amending the e-mail and url on setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
+
 from setuptools import setup
 
 setup(
     name='snapshotalyzer-30000',
     version='0.1',
-    author="Edvaldo Lima",
-    author_email="eddie.aluno@gmail.com",
+    author="Robin Norwood",
+    author_email="robin@acloud.guru",
     description="SnapshotAlyzer 30000 is a tool to manage AWS EC2 snapshots",
     license="GPLv3+",
     packages=['shotty'],
-    url="https://github.com/Eddie-coder/snapshotalyzer-30000",
-    install_requires=[
-        'click',
-        'boto3'
-    ],
+    url="https://github.com/robin-acloud/snapshotalyzer-30000",
+    install_requires=['click','boto3'],
     entry_points='''
-    [console_scripts]
-    shotty=shotty:cli
+        [console_scripts]
+        shotty=shotty.shotty:cli
     ''',
 )
